cf1c07ea8c5ef319d0f133728dd116647dd4aa0d
ingojoseph <ingo.joseph@gmail.com>
2014-03-02 16:15:30 +0100

    Create labelit.js

---
+++ labelit.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var inputs = document.querySelectorAll(".input");
+    Array.prototype.forEach.call(inputs, function(input, i){
+        var label = document.createElement("label");
+        var text = document.createTextNode(input.getAttribute("placeholder"));
+        label.appendChild(text);
+        input.parentNode.insertBefore(label, input);
+        addClass(label, "hide");
+        addClass(label, "label");
+        label.setAttribute("for", input.getAttribute("id"));
+        
+        input.addEventListener("input", function(){
+            if(this.value != ""){
+                removeClass(this.previousSibling, "hide");
+            } else if(this.value == ""){
+                addClass(this.previousSibling, "hide");
+            }
+        });
+    });
+});
+
+function addClass(el, className){  
+    if (el.classList)
+        el.classList.add(className);
+    else
+        el.className += ' ' + clasNames;
+}
+function removeClass(el, className){
+    if (el.classList)
+      el.classList.remove(className);
+    else
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
